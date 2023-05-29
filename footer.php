8aac79d73e396aa693d99c7b659c31a6d8ffdf77
kyawoo-23 <kyawkyawjek@gmail.com>
2023-05-29 20:47:53 +0630

    add slideshow

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
 
 <script type="text/javascript" src="./assets/js/navbar-toggle.js" defer></script>
 <script type="text/javascript" src="./assets/js/dialog-toggle.js" defer></script>
+<script type="text/javascript" src="./assets/js/slideshow.js" defer></script>
+
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -4,7 +4,86 @@
     include('header.php');
 
 ?>
-<div style="height: 200vh"></div>
+
+<main>
+    <div class="hero">
+        <section class="slideshow">
+            <div class="slider">
+                <div class="slide-track track-1" onmouseover="delayAnimation(this)"
+                    onmouseout="clearInterval(lastInterval)">
+                    <div class="slide">
+                        <img src="./assets/static/images/slide1.jpg" alt="slide 1" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide2.jpg" alt="slide 2" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide3.jpg" alt="slide 3" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide4.jpg" alt="slide 4" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide5.jpg" alt="slide 5" />
+                    </div>
+
+                    <div class="slide">
+                        <img src="./assets/static/images/slide1.jpg" alt="slide 1" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide2.jpg" alt="slide 2" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide3.jpg" alt="slide 3" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide4.jpg" alt="slide 4" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide5.jpg" alt="slide 5" />
+                    </div>
+                </div>
+            </div>
+            <div class="slider">
+                <div class="slide-track track-2" onmouseover="delayAnimation(this)"
+                    onmouseout="clearInterval(lastInterval)">
+                    <div class="slide">
+                        <img src="./assets/static/images/slide6.jpg" alt="slide 6" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide7.jpg" alt="slide 7" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide8.jpg" alt="slide 8" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide9.jpg" alt="slide 9" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide10.jpg" alt="slide 10" />
+                    </div>
+
+                    <div class="slide">
+                        <img src="./assets/static/images/slide6.jpg" alt="slide 6" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide7.jpg" alt="slide 7" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide8.jpg" alt="slide 8" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide9.jpg" alt="slide 9" />
+                    </div>
+                    <div class="slide">
+                        <img src="./assets/static/images/slide10.jpg" alt="slide 10" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
+
 <?php 
     include('footer.php');
 ?>
